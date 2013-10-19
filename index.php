51536c705ded43fd022ba26ea3390175d8cc9776
Krishna Ramachandran <krishnadrum@gmail.com>
2013-10-19 00:37:19 -0400

    Added .css and .js to header as well as meta data
    And added CSS JS, Img folders to ccrex.

--- index.php
+++ index.php
@@ -24,7 +24,15 @@ function analyzeFile($file_) {
 
 <html>
 	<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>CCREX</title>
 
+  
+  <link rel="stylesheet" href="css/foundation.css">
+  
+
+  <script src="js/vendor/custom.modernizr.js"></script>
 	</head>
 
 	<body>
